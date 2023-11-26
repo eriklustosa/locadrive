651c68d96fbdc091865795bde0d7ad8085dc804c
eriklustosa <123123943+eriklustosa@users.noreply.github.com>
2023-11-26 15:30:06 -0300

    Add files via upload

---
+++ pessoa.py
@@ -0,0 +1,11 @@
+class Pessoa:
+    def __init__(self, nome, data_nascimento, cpf, telefone, email, cep, estado, cidade, senha):
+        self._nome = nome
+        self._data_nascimento = data_nascimento
+        self._cpf = cpf
+        self._telefone= telefone
+        self._email = email
+        self._cep = cep
+        self._estado = estado
+        self._cidade = cidade
+        self._senha = senha
