a716f1d49965a5a85aa7d72049771f1090dbdf48
crisly <108693780+crislymss@users.noreply.github.com>
2023-12-14 11:22:09 -0300

    adicionando documentacao no servidor

--- Servidor/servidorlocadrive.py
+++ Servidor/servidorlocadrive.py
@@ -42,139 +42,59 @@ sql2 = '''
 cursor.execute(sql1)
 cursor.execute(sql2)
 
-'''class Pessoa:
-    def __init__(self, nome, data_nascimento, cpf, telefone, email, cep, estado, cidade, senha):
-        self._nome = nome
-        self._data_nascimento = data_nascimento
-        self._cpf = cpf
-        self._telefone= telefone
-        self._email = email
-        self._cep = cep
-        self._estado = estado
-        self._cidade = cidade
-        self._senha = senha
-
-class Cadastro:
-
-    def cadastra(self, pessoa):
-        existe = self.busca(pessoa)
-        if existe:
-            insert_sql = """
-            INSERT INTO cadastrapessoa (nome, cpf, email, estado, telefone, cep, cidade, data_nascimento, senha)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-            """
-            with self.conexao.cursor() as cursor:
-                cursor.execute(insert_sql, (pessoa._nome, pessoa._cpf, pessoa._email, pessoa._estado,
-                                            pessoa._telefone, pessoa._cep, pessoa._cidade, pessoa._data_nascimento, pessoa._senha))
-            conexao.commit()
-            return True
-        else:
-            return False
-
-    def busca(self, pessoa):
-        #Verifica se o CPF já existe
-        select_sql = "SELECT * FROM cadastrapessoa WHERE cpf = %s"
-        cursor.execute(select_sql, (pessoa._cpf,))
-        result = cursor.fetchone()
-        
-        #Se o CPF já existir, retorna False
-        if result:
-            return False
-        else:
-            return True
-
-    def buscausuario(self, cpf, senha):
-    
-        query = "SELECT * FROM cadastrapessoa WHERE cpf = %s AND senha = %s"
-        values = (cpf, senha)
-
-        cursor.execute(query, values)
-
-        result = cursor.fetchone()
-
-        if result:
-            return True
-        else:
-            return False
-
-    def busca_dados(self, cpf):
-        sql = "SELECT cpf, data_nascimento, nome FROM cadastrapessoa WHERE cpf = %s"
-        cursor.execute(sql, (cpf,))
-
-        resultado = cursor.fetchone()
-
-        if resultado:
-            cpf, data_nascimento, nome = resultado
-            return True, cpf, data_nascimento, nome
-        else:
-            return None
-        
-    def busca_dadosPessoais(self, cpf):
-        sql = "SELECT nome, cpf, email, estado, telefone, cep, cidade, data_nascimento FROM cadastrapessoa WHERE cpf = %s"
-        cursor.execute(sql, (cpf,))
-        
-        resultado = cursor.fetchone()
-        if resultado:
-            nome, cpf, email, estado, telefone, cep, cidade, data_nascimento = resultado
-            return nome, cpf, email, estado, telefone, cep, cidade, data_nascimento
-        else:
-            None
-
-
-# módulo cadastra_cnh.py
-
-
-class Cadastra_CNH:
-#nome, datanasci, cpf, rg, tipo_carteira, data_emissao_CNH, estado, cidade, data_venci
-    def cadastracnh(self, cnh):
-        existe = self.busca(cnh)
-        if existe == True:
-            insert_sql = """
-            INSERT INTO cadastracnh (nome, data_nascimento, cpf, rg, tipo_carteira, data_emissao_CNH, estado, cidade, data_venci)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-            """
-            cursor.execute(insert_sql, (cnh._nome, cnh._data_nascimento, cnh._cpf, cnh._rg, cnh._tipo_carteira, cnh._data_emissao_CNH, cnh._estado, cnh._cidade, cnh._data_venci))
-            conexao.commit()
-            return True
-        else:
-            return False
-    
-    def busca(self, cnh):
-        #Verifica se o RG já existe
-        select_sql = "SELECT * FROM cadastracnh WHERE rg = %s"
-        cursor.execute(select_sql, (cnh._rg,))
-        result = cursor.fetchone()
-        
-        #Se o CPF já existir, retorna False
-        if result:
-            return False
-        else:
-            return True
-    
-    def buscaDadosCNH(self, cpf):
-        sql = "SELECT rg, tipo_carteira, data_emissao_CNH, estado, cidade, data_venci FROM cadastracnh WHERE cpf = %s"
-        cursor.execute(sql, (cpf,))
-        
-        resultado = cursor.fetchone()
-        if resultado:
-            rg, tipo_carteira, data_emissao_CNH, estado, cidade, data_venci = resultado
-            return True, rg, tipo_carteira, data_emissao_CNH, estado, cidade, data_venci
-        else:
-            None'''
-
-
-
-# módulo servidor_cadastro.py
-
 
 class ServidorCadastro(threading.Thread):
+    """
+    Esta é a classe Servidor Cadastro 
+    
+    ...
+    
+    Attributes:
+    -----------
+    csocket : socket
+        Socket do cliente conectado.
+    addr: tuple
+        Endereço do cliente conectado.
+    
+    Methods:
+    _init_(self, clientAddress, clientesocket): 
+        Inicializa a classe, recebendo o endereço e o socket do cliente conectado.
+    run(self): 
+        Executa o loop principal da thread, responsável por processar as solicitações do cliente.
+    verificar_usuario_senha(self, cpf, senha): 
+        verifica se o usuário e a senha fornecidos pelo cliente são válidos.
+    cadastrar_novo_usuario(self, nome, cpf, email, estado, telefone, cep, cidade, data_nascimento, senha):
+        cadastra um novo usuário no sistema.
+    busca(self, cpf):
+        busca um usuário no sistema pelo CPF.
+    
+
+    """    
     def __init__(self, clientAddress, clientesocket):
+        """
+        Responsavel por inicializar a classe, recebendo o endereco e o socket do cliente conectado.
+        
+        ....
+        
+        """
         threading.Thread.__init__(self)
         self.csocket = clientesocket
         self.addr = clientAddress
         print('Nova conexão:', clientAddress)
         
     def run(self):
+        """
+        Este método é responsável por executar o loop principal da thread, processando as solicitações do cliente.
+        
+        ...
+        
+        
+        Returns:
+        --------
+        
+        None que significa que não há retorno.
+        
+        """
         try:
             data = self.csocket.recv(1024).decode()
             operacao, *dados = data.split(';')
@@ -240,6 +160,27 @@ class ServidorCadastro(threading.Thread):
             self.csocket.close()
         
     def verificar_usuario_senha(self, cpf, senha):
+        """Este método verifica se o usuário e a senha fornecidos pelo cliente são válidos.
+        ...
+        
+        Attributes:
+        -----------
+
+        cpf: str
+            CPF do usuário.
+        senhaa: str
+            Senha do usuário.
+        
+        Returns:
+        --------
+        
+        True: 
+            Se o usuário e a senha forem válidos.
+        False:
+            Se o usuário e a senha forem inválidos.
+                   
+                   
+        """
         query = "SELECT * FROM cadastrapessoa WHERE cpf = %s AND senha = %s"
         values = (cpf, senha)
 
@@ -253,6 +194,44 @@ class ServidorCadastro(threading.Thread):
             return False
 
     def cadastrar_novo_usuario(self, nome, cpf, email, estado, telefone, cep, cidade, data_nascimento, senha):
+        """
+        Este método cadastra um novo usuário no sistema.
+        
+        ...
+        
+        Attributes:
+        -----------
+        
+        nome: str
+            Nome do usuário.
+        cpf: str
+            CPF do usuário.
+        email: str
+            Email do usuário.
+        estado: str
+            Estado do usuário.
+        telefone: str
+            Telefone do usuário.
+        cep: str
+            CEP do usuário.
+        cidade: str
+            Cidade do usuário.
+        data_nascimento: str
+            Data de nascimento do usuário.
+        senha: str
+            Senha do usuário.
+        
+        
+        Returns:
+        --------
+        
+        True: 
+            Se o cadastro for bem-sucedido.
+        False: 
+            Se o cadastro não for bem-sucedido.
+        
+        
+        """
         try:
             insert_sql = """
             INSERT INTO cadastrapessoa (nome, cpf, email, estado, telefone, cep, cidade, data_nascimento, senha)
@@ -268,6 +247,39 @@ class ServidorCadastro(threading.Thread):
             return False
         
     def busca(self, cpf):
+        
+        """
+        Este método busca um usuário no sistema pelo CPF.
+        
+        ...
+        
+        Attributes:
+        cpf: str
+            CPF do usuário.
+        
+        
+
+        Returns:
+        --------
+        
+        nome (str): 
+            Nome do usuário.
+        cpf_encontrado (str): 
+            CPF do usuário.
+        email (str):
+            Email do usuário.
+        estado (str):
+            Estado do usuário.
+        telefone (str): 
+            Telefone do usuário.
+        cep (str):  
+            CEP do usuário.
+        cidade (str): 
+            Cidade do usuário.
+        data_nascimento (str):
+            Data de nascimento do usuário.
+        
+        """
         sql = "SELECT nome, cpf, email, estado, telefone, cep, cidade, data_nascimento FROM cadastrapessoa WHERE cpf = %s"
         cursor.execute(sql, (cpf,))
         
